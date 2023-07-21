8d88819830626958ac3d3b9829c2096bfcd11451
manu <simonotmanuel@gmail.com>
2023-07-21 19:14:56 +0200

    Moved the initialization of the root item to FileExplorerTree instead of page.svelte. FileExplorerTree must be a separate, well encapsulated component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,5 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import {
-        FSItemAPI,
-        fsItemStore
-    } from "$lib/api/FSItemAPI";
-    import TreeView from "$lib/components/TreeView.svelte";
-    import { mockRootItem } from "../tests/cypress/fixtures/fsItemFixture1";
-    import type { FSItem } from "$lib/models/FSItem";
     import FileExplorerTree from "$lib/components/FileExplorerTree.svelte";
-
-    onMount(() => {
-        // HACK: originally, the cypress line cy.get('[data-test="renameButton"]').click(); didn't work.
-        // I end up understanding that it was a kind of "race" condition:
-        // indeed the on:click handler wasn't yet positioned on the "renameButton" DOM element
-        // at the time cypress was clicking it (in Svelte terminology, the DOM was not yet "hydrated")
-
-        // SO, I had to add this global attribute to this Svelte page, that is set to the value "OK" 
-        // to signal that Svelte has finished mounting the component
-        (document.getElementById("data-test-cypress-wait-for-svelte-hydratation") as HTMLInputElement).value = "OK";
-
-        FSItemAPI.setRootItem(mockRootItem);
-    });
 
     function mainDebugButton() {   
         //FSItemAPI.add();
